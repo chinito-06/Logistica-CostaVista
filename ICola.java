9be55a644702595fd25847889f6784dfc7127e65
Juan Segundo Olazabal <juanseolazabal06@gmail.com>
2026-06-19 16:13:29 -0300

    agrego mis archivos

---
+++ ICola.java
@@ -0,0 +1,10 @@
+public interface ICola<T> {
+    public void encolar(T elemento);
+    public T desencolar();
+    public boolean estaVacia();
+    public boolean estaLlena();
+    public T frente();
+    public T fin();
+    public int tamano();
+    public void mostrar();
+}
